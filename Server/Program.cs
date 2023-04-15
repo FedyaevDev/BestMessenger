1cb8318280501ecebd5ef1ed74f7f6c0b2f6252a
FedayevR <fedayevDev@yandex.ru>
2023-04-15 23:11:23 +0700

    напсиали серверную часть приложения, 1-я итерация

--- Server/Program.cs
+++ Server/Program.cs
@@ -13,14 +13,30 @@ namespace Server
     {
         static void Main(string[] args)
         {
+            string serverMessage = "";
+            string clientMessage = "";
             TcpListener tcpServer = new TcpListener(IPAddress.Parse("127.0.0.1"),5000);
             tcpServer.Start();
+
             Console.WriteLine("Сервер запущен!!!");
-            var client = tcpServer.AcceptSocket();
+
+            Socket client = tcpServer.AcceptSocket();
+
             Console.WriteLine($"Клиент подключился {client.RemoteEndPoint}");
+
             NetworkStream stream = new NetworkStream(client);
+
             StreamWriter sw = new StreamWriter(stream);
             StreamReader sr = new StreamReader(stream);
+
+            while (true)
+            {
+                if (client.Connected == true)
+                {
+                    serverMessage = sr.ReadToEnd();
+                    Console.WriteLine("клиент: " + serverMessage);
+                }
+            }
         }
     }
 }
