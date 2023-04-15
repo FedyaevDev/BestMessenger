81ab2669774f9fa32546c293e2fee9cd79de5793
FedayevR <fedayevDev@yandex.ru>
2023-04-16 00:04:40 +0700

    исправил ошибки

--- ConsoleApp27/Program.cs
+++ ConsoleApp27/Program.cs
@@ -15,23 +15,41 @@ namespace ConsoleApp27
         {
             string clientMessage = "";
             string serverMessage = "";
-            
-            TcpClient server = new TcpClient("127.0.0.1", 5000); /// server переменная для соединения с сервером на стороне клиента
-            Console.WriteLine("Успешно подключились");
 
-            NetworkStream stream = server.GetStream();
-
-            StreamWriter sw = new StreamWriter(stream);
-            StreamReader sr = new StreamReader(stream);
-
-            while (true)
+            try
             {
-                serverMessage = Console.ReadLine();
-                sw.WriteLine(serverMessage);
-                sw.Flush();
-                clientMessage = sr.ReadToEnd();
-                Console.WriteLine("server: " + clientMessage);
+                TcpClient server = new TcpClient("127.0.0.1", 5000); /// server переменная для соединения с сервером на стороне клиента
+                Console.WriteLine("Успешно подключились");
+
+                NetworkStream stream = server.GetStream();
+
+                StreamWriter sw = new StreamWriter(stream);
+                StreamReader sr = new StreamReader(stream);
+
+                while (true)
+                {
+                    serverMessage = Console.ReadLine();
+                    if (serverMessage == "exit")
+                    {
+                        Console.WriteLine("Вы прекратили общение с сервером");
+                        server.Close();
+                        sw.Close();
+                        sr.Close();
+                        server.Close();
+                        break;
+                    }
+                    sw.WriteLine(serverMessage);
+                    sw.Flush();
+                    clientMessage = sr.ReadLine();
+                    Console.WriteLine("server: " + clientMessage);
+                }
             }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
         }
     }
 }
--- Server/Program.cs
+++ Server/Program.cs
@@ -11,35 +11,57 @@ namespace Server
 {
     public class Program
     {
+
         static void Main(string[] args)
         {
             string serverMessage = "";
             string clientMessage = "";
-            TcpListener tcpServer = new TcpListener(IPAddress.Parse("127.0.0.1"),5000);
-            tcpServer.Start();
-
-            Console.WriteLine("Сервер запущен!!!");
-
-            Socket client = tcpServer.AcceptSocket();
-
-            Console.WriteLine($"Клиент подключился {client.RemoteEndPoint}");
-
-            NetworkStream stream = new NetworkStream(client);
-
-            StreamWriter sw = new StreamWriter(stream);
-            StreamReader sr = new StreamReader(stream);
-
-            while (true)
+            try
             {
-                if (client.Connected == true)
+                TcpListener tcpServer = new TcpListener(IPAddress.Parse("127.0.0.1"), 5000);
+                tcpServer.Start();
+
+                Console.WriteLine("Сервер запущен!!!");
+
+                Socket client = tcpServer.AcceptSocket();
+
+                Console.WriteLine($"Клиент подключился {client.RemoteEndPoint}");
+
+                NetworkStream stream = new NetworkStream(client);
+
+                StreamWriter sw = new StreamWriter(stream);
+                StreamReader sr = new StreamReader(stream);
+
+                while (true)
                 {
-                    serverMessage = sr.ReadToEnd();
-                    Console.WriteLine("клиент: " + serverMessage);
-                    clientMessage = Console.ReadLine();
-                    sw.WriteLine(clientMessage);
-                    sw.Flush();
+                    if (client.Connected == true)
+                    {
+                        serverMessage = sr.ReadLine();
+                        Console.WriteLine("клиент: " + serverMessage);
+                        clientMessage = Console.ReadLine();
+                        if (clientMessage == "exit")
+                        {
+                            stream.Close();
+                            sw.Close();
+                            sr.Close();
+                            tcpServer.Stop();
+                            client.Close();
+                            Console.WriteLine("Клиент отключился");
+                            break;
+                        }
+                        sw.WriteLine(clientMessage);
+                        sw.Flush();
+                    }
                 }
             }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+           
+
+            Console.ReadLine();
         }
     }
 }
